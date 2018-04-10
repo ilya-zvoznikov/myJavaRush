ec70b27a87d6854e65316b122c5560c063f30757
Ilya Zvoznikov <constantinovich@gmail.com>
2018-04-10 12:04:01 +0700

    commit 10.04.2018

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task30.task3008.client;
+
+import com.javarush.task.task30.task3008.Connection;
+
+public class Client {
+    public class SocketThread extends Thread {
+
+    }
+
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+}
